1b78c64422e385cd44b0f46ff31c18afda45dbbb
RTJ <shreyanshece1041@gmail.com>
2019-09-05 13:21:26 +0530

    Logging In/Out

--- users/views.py
+++ users/views.py
@@ -8,10 +8,10 @@ def register(request):
         if form.is_valid():
             form.save()  #Saving the user, also hashes pwd and does all backend stuff
             username = form.cleaned_data.get('username') #username data will be in the cleaned_data dictionary
-            messages.success(request, f'Account created for { username }!') 
+            messages.success(request, f'Your Account has been created, You can login now') 
             
             #after the above the user has to be redirected to the next page(home page here) so her we go, 
-            return redirect('blog-home')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
