a413eaed94a86f12fdfa958f3f0567eb34f809b4
RTJ <shreyanshece1041@gmail.com>
2019-09-05 19:31:15 +0530

    Update User Profile

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm): #UserRegisterForm is inheriting features form UserCreationForm
     email = forms.EmailField()
@@ -8,3 +9,15 @@ class UserRegisterForm(UserCreationForm): #UserRegisterForm is inheriting featur
     class Meta: #class meta gives us a nested name space for config, and keeps config in one place
         model = User #form.save will save to this User model
         fields = ['username','email','password1','password2'] #passwd1 is input pwd and passwd2 is confirmation pwd
+
+class UserUpdateForm(forms.ModelForm): #form for user attribute model
+    email = forms.EmailField()
+
+    class Meta: 
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required #Importing login_required decorator
-from .forms import UserRegisterForm 
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm 
 
 def register(request):
     if request.method == 'POST':
@@ -16,6 +16,39 @@ def register(request):
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
-@login_required  #Builtin Django Decorator to put a check on opening the profile page manually if user is not logged in
+@login_required  #Built-in Django Decorator to put a check on opening the profile page manually if user is not logged in
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST,
+                                 instance=request.user) #creating instances of the forms here
+        
+        p_form = ProfileUpdateForm(request.POST,
+                                    request.FILES,
+                                    instance=request.user.profile) 
+    #Along with the form data there will be additional data ie, pics so it is file data, 
+    #has to be passed above before instance to be acknowledged 
+
+        if u_form.is_valid() and p_form.is_valid():  #Check validity and save the form data to DB
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated !') 
+            return redirect('profile')
+            #it is being redirected above to profile instead of going to return render below,
+            #becoz of something called "POST_GET_REDIRECT_PATTERN" 
+
+    else:
+        u_form = UserUpdateForm(instance=request.user) 
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    #To pass this form into the template we create here a context(sort of API)
+    context = {  
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'users/profile.html', context) #Passing context into template so that we can access that form
+
+'''Post-Redirect-Get. The post / redirect / get pattern or PRG pattern is a
+ development approach that prevents duplicate content when submitting forms
+  and provides a more intuitive user interface.'''
+
