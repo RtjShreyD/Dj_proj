83c79d5f6a1822fc4c53b241fdc344000659f393
RTJ <shreyanshece1041@gmail.com>
2019-09-06 18:48:16 +0530

    Create post

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User #Importing user who will be authors, users are a different table created by Django 
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -11,3 +12,7 @@ class Post(models.Model):
     #For making the post object more descriptive(while calling) we create a dunder(___) function
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self): #This function creates a GET absolute url method that returns the path of post-detail, could also be set to home
+        return reverse('blog-home', kwargs={'pk': self.pk}) #pk is the url parameter for primary key
+        #return reverse('blog-home') #Uncomment this and comment above to redirect to home
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
-from .models import Post  #to get data from DB
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Post
 
-def home(request):
+'''def home(request):
     context = {
         'posts': Post.objects.all()
     }
-    return render(request, 'blog/home.html', context)
+    return render(request, 'blog/home.html', context)'''
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' #<app>/<model>_<viewtype>.html --> Browser will use this naming convention for all class based views
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content'] #These fields are going into crispy forms
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
